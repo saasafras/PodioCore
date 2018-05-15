53ef4436067f09fcf99f15d3ab04f53915d5599e
alex <alex@brickbridgeconsulting.com>
2018-05-15 13:00:54 -0400

    PodioCore.Async 0.0.3

--- Source/PodioCore.Spaces/SpaceServiceMethods.cs
+++ Source/PodioCore.Spaces/SpaceServiceMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using PodioCore.Services;
 using PodioCore.Models;
@@ -22,5 +23,41 @@ namespace PodioCore.Spaces
             var result = await _service.GetSpace(spaceId);
             return result;
         }
-    }
+
+        public static async Task<int> CreateSpace(this Podio client, int orgId, string name, string privacy, bool autoJoin, bool postOnNewApp, bool postOnNewMember)
+		{
+			if (!initialized)
+				init(client);
+
+			var spaceId = await _service.CreateSpace(orgId, name, privacy, autoJoin, postOnNewApp, postOnNewMember);
+			return spaceId;
+		}
+
+		public static async Task<dynamic> UpdateSpace(this Podio client, int spaceId, string name = null, string urlLabel = null, string privacy = null, bool? autoJoin = null, bool? postOnNewApp = null, bool? postOnNewMember = null)
+        {
+            if (!initialized)
+                init(client);
+
+            var result = await _service.UpdateSpace(spaceId, name, urlLabel, privacy, autoJoin, postOnNewApp, postOnNewMember);
+            return result;
+        }
+
+        public static async Task<List<SpaceMicro>> GetOrganizationSpaces(this Podio client, int orgId)
+		{
+			if (!initialized)
+                init(client);
+
+			var result = await _service.GetOrganizationSpaces(orgId);
+			return result;
+		}
+
+		public static async Task<List<SpaceMicro>> GetAvailableSpaces(this Podio client, int orgId)
+        {
+            if (!initialized)
+                init(client);
+
+            var result = await _service.GetAvailableSpaces(orgId);
+            return result;
+        }
+	}
 }
