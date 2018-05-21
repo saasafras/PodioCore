a9ddb9eecbd9ec13324f60f28e53f4a59f4324c4
alex <alex@brickbridgeconsulting.com>
2018-05-21 14:41:08 -0400

    new auth extensions

---
+++ Source/PodioCore.Auth/AuthExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PodioCore.Auth;
+using PodioCore.Utils.Authentication;
+namespace PodioCore.Auth
+{
+    public static class AuthExtensions
+    {
+		public static async Task<PodioOAuth> AuthenticateWithPassword(this Podio client, string clientId, string clientSecret, string username, string password)
+        {
+			var authRequest = new Dictionary<string, string>()
+			{
+				{"client_id",clientId},
+				{"client_secret",clientSecret},
+				{"username", username},
+				{"password", password},
+				{"grant_type", "password"}
+			};
+			PodioOAuth podioOAuth = await client.Post<PodioOAuth>("/oauth/token", authRequest, true).ConfigureAwait(false);
+            client.OAuth = podioOAuth;
+            client.AuthStore.Set(podioOAuth);
+
+            return podioOAuth;
+        }
+    }
+}
