cdaffc8ae50b42c7fe63447bfbbf24998764fa9b
alex <alex@brickbridgeconsulting.com>
2018-11-18 15:47:56 -0500

    OneUseTokenProvider

---
+++ Source/PodioCore.Auth/OneUseTokenProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PodioCore.Auth;
+using System.Net.Http;
+using PodioCore.Utils.Authentication;
+using System;
+namespace PodioCore.Auth
+{
+	public class OneUseAccessTokenProvider : IAccessTokenProvider
+    {
+        public OneUseAccessTokenProvider(string accessToken)
+        {
+            _accessToken = accessToken;
+        }
+        private string _accessToken;
+
+        public PodioAccessToken TokenData => new PodioAccessToken { AccessToken = _accessToken };
+    }
+}
