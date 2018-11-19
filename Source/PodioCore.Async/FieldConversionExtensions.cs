a70c65d294858863b04e125321a486fb643ca8bd
alex <alex@brickbridgeconsulting.com>
2018-11-19 01:24:59 -0500

    Get Specific ApplicationField type w/o Generic

---
+++ Source/PodioCore.Async/FieldConversionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PodioCore.Models;
+using PodioCore.Utils.ApplicationFields;
+
+namespace PodioCore.Async
+{
+    public static class FieldConversionExtensions
+    {
+        public static AppReferenceApplicationField GetAppReferenceAppField(this Application app, int fieldId)
+        {
+            var field = app.Field<AppReferenceApplicationField>(fieldId);
+            return field;
+        }
+    }
+}
